f67340c5af2669a17bfb0e46e8569757d849014a
hugomago <hugo_mago_7@hotmail.com>
2020-06-25 17:36:58 -0500

    modificando listar.php

--- listar.php
+++ listar.php
@@ -2,9 +2,17 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Lista de Alumnos</title>
   </head>
   <body>
-    
+    <head>
+      <h1>Alumnos</h1>
+    </head>
+    <body>
+
+    </body>
+    <footer>
+
+    </footer>
   </body>
 </html>
