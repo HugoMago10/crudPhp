eaba6d33f7f16053adcc58a07d6227692738e03d
hugomago <hugo_mago_7@hotmail.com>
2020-06-26 00:27:19 -0500

    cambio1

--- listar.php
+++ listar.php
@@ -1,3 +1,11 @@
+<?php
+  include ("conexion.php");
+  //Cabecera tiene el mismo nombre que la columna de la BD para poder recorrer
+  //campo del registro
+  $cabecera = ['id_alumno','nombre','apePat','apePat','fecha_nac','calle','numero',
+    'colonia','municipio','estado'];
+  $registros = getListaAlumno(); //Obtengo el arreglo de registros
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -5,14 +13,39 @@
     <title>Lista de Alumnos</title>
   </head>
   <body>
-    <head>
+    <header>
       <h1>Alumnos</h1>
-    </head>
-    <body>
+    </header>
+    <main>
+      <p><a href="nuevo.php">Nuevo registro</a></p>
 
-    </body>
+      <table border="1">
+        <thead>
+          <tr>
+            <?php
+            //imprime las cabeceras
+              for ($i=0; $i <count($cabecera); $i++) {
+                printf ("<th>%s</th>",$cabecera[$i]);
+              }
+            ?>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            //obtengo los registros
+            for ($i = 0; $i < count($registros); $i++) {
+              echo ("<tr>");
+              //Obtengo cada columna del registro
+              for ($j=0; $j <count($cabecera); $j++) {
+                printf ("<td>  %s </td>", $registros[$i][$cabecera[$j]]);
+              }
+              echo ("</tr>");
+            }
+          ?>
+        </tbody>
+      </table>
+    </main>
     <footer>
-
     </footer>
   </body>
 </html>
